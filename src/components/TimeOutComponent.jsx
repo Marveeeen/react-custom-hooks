3c04c024627aeef2cee976f11a3fa8c0923eb9ea
Marveeen <jmvroqueee@gmail.com>
2022-09-29 23:57:28 +0800

    Implemented useTimeOut hook in the TimeOutComponent

--- src/components/TimeOutComponent.jsx
+++ src/components/TimeOutComponent.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import useTimeOut from "../hooks/useTimeOut";
 
 export default function TimeOutComponent() {
     const [count, setCount] = useState(10)
+    const { clear, reset } = useTimeOut(() => setCount(0), 100)
 
     return(
         <div>
             <div>{count}</div>
             <button onClick={() => setCount(c => c + 1)}>Increment</button>
-            <button>Increment</button>
-            <button>Increment</button>
+            <button onClick={clear}>Clear Timeout</button>
+            <button onClick={reset}>Reset Timeout</button>
         </div>
     )
 }
