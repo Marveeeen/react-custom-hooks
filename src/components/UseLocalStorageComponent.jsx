8668c737e02e77e66a5b8c6880593fa47e7796b5
Marveeen <jmvroqueee@gmail.com>
2022-10-01 22:38:56 +0800

    Add useUpdateLogger hook

--- src/components/UseLocalStorageComponent.jsx
+++ src/components/UseLocalStorageComponent.jsx
@@ -1,7 +1,10 @@
 import useLocalStorage from "../hooks/useLocalStorage"
+import useUpdateLogger from "../hooks/useUpdateLogger"
 
 export default function UseLocalStorage() {
     const [name, setName] = useLocalStorage('name', '')
+    useUpdateLogger(name)
+
     return(
         <form>
             <label htmlFor="name">Name</label>
